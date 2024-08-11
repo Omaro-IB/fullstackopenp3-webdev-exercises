ee8e71c1b9469d872d08151ee263fa2ecc4092b2
Omar I <omarom.ibra@gmail.com>
2024-08-12 00:30:45 +0300

    exercises 3.9-3.11: completed backend (with SSL) and updated frontend to support

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const  morgan = require('morgan')
-const app = express()
+const cors = require('cors')
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const privateKey = fs.readFileSync('REDACTED');
+const certificate = fs.readFileSync('REDACTED');
+const credentials = {key: privateKey, cert: certificate};
+
+const app = express();
+
 // Persons list - statically defined
 let persons = [
     {
@@ -27,6 +37,8 @@ let persons = [
 
 // JSON middleware
 app.use(express.json())
+// CORS middleware
+app.use(cors())
 
 // Morgan middleware for logging - request body logged for POST *only*
 morgan.token('body', (req, _) => JSON.stringify(req.body))
@@ -100,8 +112,17 @@ app.post('/api/persons', (req, res) => {
 const unknownEndpointMiddleware = (request, response) => {response.status(404).send({ error: 'unknown endpoint' })}
 app.use(unknownEndpointMiddleware)
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-    console.log(`http://127.0.0.1:${PORT}`)
+// const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+// const PORT_HTTP = 80
+const PORT_HTTPS = 4441
+const HOSTNAME = '10.0.0.208'
+// httpServer.listen(PORT_HTTP, HOSTNAME, () => {
+//     console.log(`HTTP server running on port ${PORT_HTTP}`)
+//     console.log(`http://${HOSTNAME}:${PORT_HTTP}`)
+// })
+httpsServer.listen(PORT_HTTPS, HOSTNAME, () => {
+    console.log(`HTTPS server running on port ${PORT_HTTPS}`)
+    console.log(`https://${HOSTNAME}:${PORT_HTTPS}`)
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from 'react'
+import phoneService from "./services/phoneService.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Filter component
+const Filter = ({newFilter, setNewFilter}) => (
+    <div> filter shown with
+        <input value={newFilter} onChange={(e) => {setNewFilter(e.target.value)}}></input>
+    </div>
+)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//  Add new person form
+const PersonForm = ({newName, setNewName, newNumber, setNewNumber, handleClick}) => (
+    <form>
+        <div> name: <input value={newName} onChange={(e) => {setNewName(e.target.value)}}></input></div>
+        <div> number: <input value={newNumber} onChange={(e) => {setNewNumber(e.target.value)}}></input></div>
+        <div> <button type="submit" onClick={handleClick}>add</button> </div>
+    </form>
+)
+
+// A single person
+const Person = ({name, number, onDelete}) => (
+    <div className={"person"} style = {{display: 'flex', flexDirection: 'row'}}>
+        <div>{name} {number}</div>
+        <div style={{width:"30px"}}> </div>
+        <button onClick={onDelete}>delete</button>
+    </div>
+)
+
+
+const App = () => {
+    // States
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setNewFilter] = useState('')
+    const [message, setMessage] = useState('')
+    const [messageType, setMessageType] = useState('notification')
+
+    // Message
+    const Message = () => {
+        if (message.length > 0) {return <div className={messageType}>{message}</div>}
+        else {return <div></div>}
+    }
+    const displayMessage = (msg, type) => {
+        setMessageType(type)
+        setMessage(msg)
+        setTimeout(() => {setMessage("")}, 3000)
+    }
+
+
+    // Refresh data
+    const refreshData = () => {
+        phoneService.getAll().then(data => {
+            setPersons(data)
+        }).catch(_ => {
+            displayMessage("Error getting data from server", "error")
+        })
+    }
+    useEffect(refreshData, []);  // refresh on first render
+
+
+    // List of persons
+    const Persons = () => {
+        const personsToShow = (newFilter === '') ? persons : persons.filter(p => (p.name.toUpperCase().includes(newFilter.toUpperCase())))
+        return (<div>{personsToShow.map(p => <Person key={p.id} name={p.name} number={p.number} onDelete={() => {
+            if (window.confirm("Do you really want to delete?")) {
+                phoneService.deletePerson(p.id).then(data => {
+                    displayMessage(`Deleted ${data.name}`, "notification")
+                    setPersons(persons.filter(p => p.id !== data.id))
+                }).catch(_ => {
+                    displayMessage(`${p.name} already deleted from server`, "error")
+                    refreshData()})
+            }
+        }}
+        ></Person>)}</div>)
+    }
+
+
+    // Handles 'add' button click
+    const handleAdd = (e) => {
+        e.preventDefault()  // prevent page reload
+        let personExists = -1  // -1 if doesn't exist, else ID of the duplicate person
+        let largestID = -1
+
+        persons.forEach(p => {
+            if (p.name === newName) {personExists = p.id}
+            if (parseInt(p.id) > largestID) {largestID = parseInt(p.id)}
+        });  // check for already-existing person & for largest ID
+
+        if (personExists === -1) {  // add person if does not already exist
+            const newPerson = {name: newName, number: newNumber, id: (largestID+1).toString()}
+            phoneService.createPerson(newPerson).then(data => {
+                setPersons(persons.concat(data))
+                displayMessage(`Added ${newName}`, "notification")
+            }).catch(_ => {
+                displayMessage("Error creating person", "error")
+            })
+        } else {  // if exists, update upon user confirmation
+            if (window.confirm(`"${newName}" is already added to the phonebook, replace the old number with the new one?`)) {
+                const newPerson = {name: newName, number: newNumber, id: personExists}
+                phoneService.updatePerson(personExists, newPerson).then(data => {
+                    setPersons(persons.map(p => {if (p.id === data.id) {return data} else {return p}}))
+                    displayMessage(`Updated ${newName}`, "notification")
+                }).catch(_ => {
+                    setPersons(persons.filter(p => p.id !== personExists))
+                    displayMessage(`${newName} already deleted from server`, "error")
+                })
+            }
+        }
+
+        setNewName(''); setNewNumber('')  // reset fields
+    }
+
+
+    return (
+        <div>
+            <Message />
+            <h1>Phonebook</h1>
+            <Filter newFilter={newFilter} setNewFilter={setNewFilter} />
+            <h2>Add New Entry</h2>
+            <PersonForm newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} handleClick={handleAdd} />
+            <h2>Numbers</h2>
+            <Persons />
+        </div>
+    )
 }
 
 export default App
